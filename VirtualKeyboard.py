e06bec9b6ac97d0f4aa9e07522e1a7bf3ee66c80
Aayush <aayushupadhyay.ak@gmail.com>
2022-08-06 17:27:00 +0530

    Initial

--- VirtualKeyboard.py
+++ VirtualKeyboard.py
@@ -12,7 +12,6 @@ cap.set(4, 1280)
 
 finaltext = ""
 keyboard = Controller()
-
 keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
         ["Z", "X", "C", "V", "B", "N", "M", ",",".", "/" ]]
